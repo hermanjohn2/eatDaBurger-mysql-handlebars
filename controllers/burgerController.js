16cbf3e832aaa53c7dae20aab07ec287a7adc1c7
John Herman <hermanjohn2@gmail.com>
2020-05-27 16:56:38 -0400

    wrote code to get and post burgers through the controller

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -14,4 +14,36 @@ router.get('/', (req, res) => {
   });
 });
 
+router.post('/api/burgers', (req, res) => {
+  // console.log(req.body);
+
+  burger.create(
+    ['name', 'isDevoured'],
+    [req.body.name, req.body.isDevoured],
+    (result) => {
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+  // console.log(req.params.id);
+  let condition = `id = ${req.params.id}`;
+
+  burger.update(
+    {
+      isDevoured: req.body.isDevoured
+    },
+    condition,
+    (result) => {
+      // console.log(result);
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else res.status(200).end();
+    }
+  );
+
+  res.end();
+});
+
 module.exports = router;
