f1a99cdd226accd61cf49fc7aa5b2cb45733c938
John Herman <hermanjohn2@gmail.com>
2020-05-28 15:32:12 -0400

    wrote code to change isDevoured state of list item using an ajax call to burgers API. burgers will now change lists based on isDevoured state.

---
+++ public/assets/js/app.js
@@ -0,0 +1,28 @@
+// Using ES5 syntax because ES6 arrow functions will not work to use .data()
+$(document).ready(function () {
+  $('.burger-handler').on('click', function (event) {
+    // console.log('click');
+
+    var id = $(this).data('id');
+    var devouredState = $(this).data('devoured');
+
+    // console.log(`id: ${id}`, `isDevoured: ${devouredState}`);
+
+    // Checking if devouredState is true or false -- changing value based on state
+    if (devouredState) devouredState = false;
+    else devouredState = true;
+
+    var newDevouredState = {
+      isDevoured: devouredState
+    };
+
+    $.ajax(`/api/burgers/${id}`, {
+      type: 'PUT',
+      data: newDevouredState
+    }).then(function () {
+      //   console.log(`Changed Devoured State to: ${devouredState}`);
+
+      location.reload();
+    });
+  });
+});
