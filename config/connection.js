3b3089c7a837f0392bad5770973425a9eb947cbd
John Herman <hermanjohn2@gmail.com>
2020-05-29 19:06:36 -0400

    Connected project with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,26 @@
 // MySQL Connection
 const mysql = require('mysql');
+const connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'charchar',
-  database: 'burger_db'
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'charchar',
+    database: 'burger_db'
+  });
+}
+
+
 
 connection.connect((err) => {
   if (err) {
     console.log(`Error connecting: ${err.stack}`);
     return;
   }
-  console.log(`Connected as id ${connection.threadId}`);
 });
 
 // Export connection to ORM
