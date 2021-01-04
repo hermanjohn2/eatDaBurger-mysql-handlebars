2f80fbfc918c3cc173ff339f76aa11d060ab4a3c
John Herman <hermanjohn2@gmail.com>
2021-01-04 18:00:48 -0500

    Modularized functionality into smaller functions achieving the same result. Fixed ajax error. Set up nodemon for future development

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,52 +1,57 @@
 $(document).ready(function () {
-  // When the "Devour"/"Eat Again" btn is clicked...
-  $('.burger-handler').on('click', function (event) {
-    var id = $(this).data('id');
-    var devouredState = $(this).data('devoured');
+	// Calls PUT route and updates burger's devoured state in db
+	function updateBurger(id, state) {
+		$.ajax(`/api/burgers/${id}`, {
+			type: 'PUT',
+			data: state
+		}).then(function () {
+			// Reloading the DOM
+			location.reload();
+		});
+	}
 
-    // Checking if devouredState is true or false -- changing value based on state
-    if (devouredState) devouredState = false;
-    else devouredState = true;
+	// Calls POST route and creates a new burger entry in the db
+	function createBurger(name) {
+		$.ajax('/api/burgers', {
+			type: 'POST',
+			data: name
+		}).then(function () {
+			// Reloading the DOM
+			location.reload();
+		});
+	}
 
-    var newDevouredState = {
-      isDevoured: devouredState
-    };
+	// Handle's Devour and Eat Again btn clicks
+	function handleBurgerClick(event) {
+		// Checking if devouredState is true or false -- changing value based on state
+		event.preventDefault();
+		var id = $(this).data('id');
+		var devouredState = $(this).data('devoured');
 
-    // PUT request
-    $.ajax(`/api/burgers/${id}`, {
-      type: 'PUT',
-      data: newDevouredState
-    })
-      .then(function () {
-        console.log(`Changed Devoured State to: ${devouredState}`);
+		if (devouredState) devouredState = false;
+		else devouredState = true;
 
-        // Reloading the page
-        location.reload();
-      })
-      .catch(function (err) {
-        if (err) throw err;
-      });
-  });
+		var newDevouredState = {
+			isDevoured: devouredState
+		};
 
-  // When the create new burger btn is clicked...
-  $('.create-form').on('submit', function (event) {
-    event.preventDefault();
+		updateBurger(id, newDevouredState);
+	}
 
-    var newBurger = {
-      name: $('#burger').val().trim()
-    };
+	// Handles submit button click
+	function handleBurgerSubmit(event) {
+		event.preventDefault();
 
-    // POST request
-    $.ajax('/api/burgers', {
-      type: 'POST',
-      data: newBurger
-    })
-      .then(function () {
-        // Reloading the page
-        location.reload();
-      })
-      .catch(function (err) {
-        if (err) throw err;
-      });
-  });
+		var newBurger = {
+			name: $('#burger').val().trim()
+		};
+
+		createBurger(newBurger);
+	}
+
+	// When the "Devour"/"Eat Again" btn is clicked...
+	$('.burger-handler').on('click', handleBurgerClick);
+
+	// When the create new burger btn is clicked...
+	$('.create-form').on('submit', handleBurgerSubmit);
 });
